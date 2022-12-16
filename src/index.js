674fceac9b0a35823a82d60009e948190affb67d
Kayla Volk <kaylavolk8@gmail.com>
2022-12-15 23:27:00 -0500

    tasklister

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+const main = document.querySelector("main-content")
+console.log(main)
+
+const taskForm = document.querySelector("#create-task-form")
+const taskList = document.querySelector("#tasks")
+taskForm.addEventListener("submit", function(e){
+  e.preventDefault()
+  // user input
+const newTask = document.querySelector("#new-task-description").value
+// slap it on the DOM
+
+taskList.innerHTML += `
+<li> ${newTask}
+<button data-action="delete"> X </button>
+ </li>
+
+ ` 
+// const taskItem = document.createElement("li")
+// taskItem.innerText = newTask
+// taskList.appendChild(taskItem)
+taskForm.reset()
+})
+
+taskList.addEventListener("click", function(e){
+  if (e.target.dataset.action === "delete"){
+    e.target.parentElement.remove()
+  }
+})
